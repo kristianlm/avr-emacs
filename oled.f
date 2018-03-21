4a40cfb1be1ae185e7de9ebb3cd981d4f6b7dd6f
Kristian Lein-Mathisen <kristianlein@gmail.com>
2018-03-21 15:15:21 +0100

    feat: adds 'screen' concept
    it's not a great one, I'm storing text in a 32*8 charmap. that's 256
    bytes which I have. I don't have the 1Kbyte I need for the monochrome
    128x64 pixels so there you go.

--- oled.f
+++ oled.f
@@ -59,3 +59,45 @@ $ 00 >oled $ 00 >oled
 $ ff >oled $ ff >oled
 $ 00 >oled $ 00 >oled
 $ ff >oled $ ff >oled
+
+\ ========== screen ==========
+
+( screen as in textbuffer => oled )
+
+$ 100 var &tbuf ( 128*64 pixels, 4x8 font = 32 * 8 characters)
+: sclear ] &tbuf [ $ 100 l [ $ 20 l ] fill ] ;
+: sdump ] &tbuf [ $ fff0 l ] and [ $ 112 l ] dump ] ;
+
+$ 2 var cursorp
+0 cursorp !
+cursorp @ h.
+
+: oled<row ( a -- ) [ $ 0 l ] oled.x [ $ 20 l ] for ] c@+ ] oled.emit ] next ] drop ] ;
+: cursor++ ( -- ) ] cursorp ] c@ ] 1+ ] cursorp ] c! ] ;
+: >sb  ( c -- ) ] &tbuf ] cursorp ] c@ ] + ] c! ] cursor++ ] ;
+: sb.cr ] cursorp ] c@ [ $ 00e0 l ] and ] cursorp ] c! ] ;
+
+: srefresh
+    [ $ 07 l ] oled.page ] &tbuf [ $ 0000 l ] + ] oled<row
+    [ $ 06 l ] oled.page ] &tbuf [ $ 0020 l ] + ] oled<row
+    [ $ 05 l ] oled.page ] &tbuf [ $ 0040 l ] + ] oled<row
+    [ $ 04 l ] oled.page ] &tbuf [ $ 0060 l ] + ] oled<row
+    [ $ 03 l ] oled.page ] &tbuf [ $ 0080 l ] + ] oled<row
+    [ $ 02 l ] oled.page ] &tbuf [ $ 00a0 l ] + ] oled<row
+    [ $ 01 l ] oled.page ] &tbuf [ $ 00c0 l ] + ] oled<row
+    [ $ 00 l ] oled.page ] &tbuf [ $ 00e0 l ] + ] oled<row ] ;
+
+
+: oh.
+    ] dup [ $ 0c l ] rshift [ $ f l ] and ] digit ] >sb
+    ] dup [ $ 08 l ] rshift [ $ f l ] and ] digit ] >sb
+    ] dup [ $ 04 l ] rshift [ $ f l ] and ] digit ] >sb
+          [ $ 00 l ] rshift [ $ f l ] and ] digit ] >sb
+    ] srefresh
+    ] ;
+
+\ : forever ] sb.cr ] o.counter ] srefresh ] forever ] ;
+\ : display ] sb.cr ] clicks ] @ ] oh. ] srefresh ] ;
+
+
+\ sclear sb.cr $ 0002 oh.
